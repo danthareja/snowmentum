24642b676d9449dd895f565eff52444dd895f5df
Dan Thareja <danthareja@gmail.com>
2014-09-11 20:02:26 -0700

    (init) initializes mocha test suite

---
+++ app/main.spec.js
@@ -0,0 +1,20 @@
+/* global main, describe, it, expect, should */
+
+describe('main()', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(main).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
